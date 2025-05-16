18e9ca6e8d4b24cff23164af80eee89970533319
shrabondas5544 <shrabondas5544@gmail.com>
2025-05-16 23:01:20 +0600

    3rd

--- Val.kt
+++ Val.kt
@@ -1,6 +1,7 @@
 
-val name: String ="Shrabon"
-var greeting: String? ="hello world" //"string?" for null type
+val name = "Shrabon"
+var greeting: String? ="Hi"
+var greetings: String? = null //"string?" for null type
 
 fun main() {
     println(greeting)
@@ -14,5 +15,32 @@ fun main() {
     println("----------------------")
     greeting = null
     println(greeting)
+    println(name)
+
+    println("---------if statement-------------")
+    if (greetings != null) {
+        println(greetings)
+    }
+    println(name)
+
+    println("----------------------")
+    greeting = "hello"
+    val greetingToPrint = if(greeting != null) greeting else "Hi"
+    println(greetingToPrint)
+    println(name)
+
+    println("----------when statement------------")
+    when (greeting) {
+        null -> println("Hello")  // in line num 16 we set greeting = null.
+        else -> println(greeting)
+    }
+    println(name)
+
+    println("----------------------")
+    val greetingToPoint = when (greeting) {
+        null -> "Hi"
+        else -> greeting
+    }
+    println(greetingToPoint)
     println(name)
 }
